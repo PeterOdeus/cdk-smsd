4460d2209e95a0c06cf6488fa6d008d341fd6791
asad <asad@ebi.ac.uk>
2009-11-18 09:19:32 +0000

    smsd module tag added Signed-off-by: asad <asad@ebi.ac.uk>

--- src/main/org/openscience/cdk/smsd/core/tools/ExtAtomContainerManipulator.java
+++ src/main/org/openscience/cdk/smsd/core/tools/ExtAtomContainerManipulator.java
@@ -47,9 +47,35 @@ import org.w3c.dom.NodeList;
 
 /**
  *
- * @author Syed Asad Rahman, EMBL-EBI, Cambridge, UK  * @contact asad@ebi.ac.uk
- * This is an extension of CDK Atom container where we have modfiled few methods
- * Some part of this code was taken from CDK source code and modified
+ * Copyright (C) 2006-2009  Syed Asad Rahman {asad@ebi.ac.uk}
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/**
+ * @cdk.module smsd
+ */
+
+/**
+ * <p>This is an extension of CDK AtomContainer.
+ * Some part of this code was taken from CDK source code and modified.</p>
  */
 public class ExtAtomContainerManipulator extends AtomContainerManipulator {
 
